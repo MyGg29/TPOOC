8a8d45c77faf6aff8d41c6c282acb094896b4ad1
gwendalVPS <gwendalgourmelon@gmail.com>
2019-10-25 14:51:05 +0200

    Question 3, utilisations du design pattern Service pour respecter Single Responsability Principle

--- app/src/controllers/OrderController.ts
+++ app/src/controllers/OrderController.ts
@@ -6,14 +6,17 @@ import {
 
 import { delAsync, getAsync, setAsync } from '../../utils/storage'
 import OrderModel  from '../models/order'
-import merge from 'deepmerge'
+import OrderService from '../services/ordersService'
 
 export default class OrderController {
   public path = '/orders'
   public router = Router()
+  public orderService = new OrderService()
 
   constructor() {
     this.intializeRoutes()
+
+
   }
 
   public intializeRoutes() {
@@ -25,66 +28,31 @@ export default class OrderController {
   }
 
   public getAll = async (request: Request, response: Response) => {
-    let orders:OrderModel.Order[] = JSON.parse(await getAsync("orders")) || []
-    response.status(200).json(orders)
+    this.orderService.getAll()
+    .then((orders:OrderModel.Order[]) => 
+          response.status(200).json(orders))
   }
 
   public create = async (request: Request, response: Response) => {
-    let orders:OrderModel.Order[] = JSON.parse(await getAsync("orders")) || []
     let newOrder:OrderModel.Order = request.body
-    
-    //get the max id of every orders stored, 0 if no orders are stored
-    let newId = 0
-    if(orders !== []){
-      //the max id of the current array of order + 1
-      newId = 1 + Math.max.apply(Math, orders.map((o:OrderModel.Order) => o.id))
-    }
-    orders.push({
-                  id:newId,
-                  createdAt:new Date(),
-                  ...newOrder
-                })
-
-    setAsync("orders",JSON.stringify(orders))
-
-    response.status(201).json(newId)
-    
+    this.orderService.create(newOrder)
+    .then((newId:number) => response.status(201).json(newId))
   }
   
   public update = async (request: Request, response: Response) => {
-    let orders:OrderModel.Order[] = JSON.parse(await getAsync("orders")) || []
     let receivedOrder:OrderModel.Order = request.body
-    let orderToUpdate:OrderModel.Order = orders.find((value:OrderModel.Order) => 
-                                                value.id.toString() === request.params.id)
-
-    //we merge the order in database with the one received, 
-    //when we try to merge and array in the structure (the package key for example), we overwrite the old one with the new one
-    //note : this allows to change the date of creation and the id
-    const overwriteMerge = (destinationArray:any, sourceArray:any, options:any) => sourceArray
-    let updatedOrder = merge(orderToUpdate, receivedOrder, {arrayMerge:overwriteMerge})
-
-    //we replace the old order with the new one, we have to get the index of the old one in order to use splice()                                            
-    let orderIndexToUpdate:number = orders.findIndex((value:OrderModel.Order) => 
-                                                value.id.toString() === request.params.id)
-    orders.splice(orderIndexToUpdate,1,updatedOrder)
-
-    setAsync("orders",JSON.stringify(orders))
-    response.sendStatus(200)
+    this.orderService.update(parseInt(request.params.id),receivedOrder)
+    .then(() => response.sendStatus(200))
   }
 
   public delete = async (request: Request, response: Response) => {
-    let orders:OrderModel.Order[] = JSON.parse(await getAsync("orders")) || []
-    let orderIndexToDelete:number = orders.findIndex((value:OrderModel.Order) => 
-                                                value.id.toString() === request.params.id)
-    orders.splice(orderIndexToDelete,1)
-    response.sendStatus(200);
+    this.orderService.delete(parseInt(request.params.id))
+    .then(() => response.sendStatus(200))
   }
 
   public get = async (request: Request, response: Response) => {
-    let orders:OrderModel.Order[] = JSON.parse(await getAsync("orders")) || []
-    
-    let queriedOrder:OrderModel.Order = orders.find((value:OrderModel.Order) => 
-                                                value.id.toString() === request.params.id)
-    response.json(queriedOrder)
+    this.orderService.get(parseInt(request.params.id))
+    .then((queriedOrder:OrderModel.Order) => 
+          response.status(200).json(queriedOrder))
   }
 }
